7dbac701850f94f081442a71fcaa9b94bf55980c
Antonio Marcos Dias Soares <atmarcos28@gmail.com>
2022-09-05 21:10:42 -0300

    PARTE 9.0

---
+++ pokedex/src/routes/coordinator.js
@@ -0,0 +1,5 @@
+ export const goToHome = (navigate) => {navigate("/")};
+export const goToPokedex = (navigate) => {navigate("/pokedex")};
+export const goToDetailPokemons = (navigate) => {navigate("/pokedex/:id")};
+
+ 
